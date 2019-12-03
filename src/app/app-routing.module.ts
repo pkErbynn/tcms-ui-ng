b14443e0df2220dc5c7e0752c5890c0bf82992fc
pkerbynn <john.erbynn@gmail.com>
2019-12-03 14:37:59 +0000

    client project list route works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'client-list', component: ClientListComponent},
+  {path: 'project-list', component: ProjectListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
