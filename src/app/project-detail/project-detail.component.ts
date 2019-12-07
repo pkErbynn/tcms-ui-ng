3d0543eccf9b8f0f81c9e1fd4555ed6952ec29aa
pkerbynn <john.erbynn@gmail.com>
2019-12-07 03:45:04 +0000

    [wip]: details shows in console log

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
+
+  public projectDetail = [];
 
   ngOnInit() {
+    this.showProjectDetails()
+  }
+
+  showProjectDetails(){
+    
+    return this.projectService.getProjects()
+    .subscribe(data => {
+      console.log(data)
+      this.projectDetail = data;
+      console.log(this.projectDetail)
+    })
   }
 
 }
